053f7efcc0fcbb21248f2a5b30662e1cf3d04dee
Maksym Hopaitsa <maksim260107@gmail.com>
2025-01-03 01:13:36 +0100

    Add nav buttons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,13 @@
+import CircleIcon from "../assets/CircleIcon.png"
+import RectangleIcon from "../assets/RectangleIcon.svg"
 
 const Nav = () => {
     return (
-        <div>
-            <span> sdsd</span>
+        <div className="flex flex-col justify-center items-center gap-3 mt-">
+            <img src={CircleIcon} alt="now"/>
+            <img src={RectangleIcon} alt="next"/>
+            <img src={RectangleIcon} alt="next"/>
+            <img src={RectangleIcon} alt="next"/>
         </div>
     );
 };
